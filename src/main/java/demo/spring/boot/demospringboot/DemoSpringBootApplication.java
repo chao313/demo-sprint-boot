34b133facade783fa69c47be9eed3d848b6102eb
wanghaichao <m18888106873@163.com>
2017-11-26 14:01:04 +0800

    删除无关代码

--- src/main/java/demo/spring/boot/demospringboot/DemoSpringBootApplication.java
+++ src/main/java/demo/spring/boot/demospringboot/DemoSpringBootApplication.java
@@ -1,13 +1,9 @@
 package demo.spring.boot.demospringboot;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@MapperScan(value = "demo.spring.boot.demospringboot.mybatis.mapper")//mybatis
-@EnableCaching//表示redis开启缓存
 public class DemoSpringBootApplication {
 
     public static void main(String[] args) {
